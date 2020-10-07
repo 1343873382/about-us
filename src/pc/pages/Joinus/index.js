4cd6a59d5405e02ded049d5c65a200594d5dc0d0
Qixing Lin <3077006505@qq.com>
2020-10-07 23:02:14 +0800

    fix:修改无法报名按纽动画

--- src/pc/pages/Joinus/index.js
+++ src/pc/pages/Joinus/index.js
@@ -5,8 +5,7 @@ import './index.scss'
 
 export const Joinus = props => {
   const { joinus } = useContext(Context)
-  const { p1, p2, p3, button, tip } = joinus
-
+  const { p1, p2, p3, button } = joinus
   return (
     <div className={`joinus page ${props.className}`}>
       {/* <div className="container2"> */}
@@ -16,7 +15,6 @@ export const Joinus = props => {
           <div className="p2">{p2}</div>
           <div className="p3">{p3}</div>
           <button>{button.text}</button>
-          <div className="tip">{tip}</div>
         </div>
         <div className="girl"></div>
       </div>
