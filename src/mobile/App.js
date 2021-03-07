16992f398335573c0443923c3880bed68b2e5c94
kying-star <3077006505@qq.com>
2021-03-07 13:59:01 +0800

    fix: 更换镜像源

--- src/mobile/App.js
+++ src/mobile/App.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2021-03-07 12:34:51
+ * @LastEditTime: 2021-03-07 13:58:40
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: /about-us/src/mobile/App.js
+ */
 import React from 'react'
 import {
   Carousel,
@@ -12,7 +20,7 @@ import "./scss/normalize.scss"
 import "./scss/global.scss"
 
 export const App = () => {
-
+// 更换 router
   return (
     <>
       <Carousel />
